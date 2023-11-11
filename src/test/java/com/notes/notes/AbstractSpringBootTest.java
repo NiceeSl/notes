ea7f82acc2c2f20e1fe10925cf1483388cba5b79
NiceeSl <gerasimov_slavik@inbox.ru>
2023-11-11 16:13:10 +0300

    added SpringProfile

--- src/test/java/com/notes/notes/AbstractSpringBootTest.java
+++ src/test/java/com/notes/notes/AbstractSpringBootTest.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("testing")
 public abstract class AbstractSpringBootTest {
 
     @Autowired
